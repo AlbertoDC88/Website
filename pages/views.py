aac47c3b4c1f44d535fb5f42a93bf6880e248ba3
Alberto Diaz <albertodiaz_cantero@hotmail.com>
2018-10-02 16:12:30 +0100

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('Welcome to Travel Made Easy, World!')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+##
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
